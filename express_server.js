446278b5d50ae47be3e62a1be1e82fae07d62135
Shanel Beebe <shanelbeebe@icloud.com>
2024-03-16 02:51:34 +0000

    Add GET route to express_server.js and render using accompanying template Add POST route to express_server.js to receive form submission Implement generate random string function

--- express_server.js
+++ express_server.js
@@ -1,14 +1,30 @@
+// global variables
 const express = require('express');
 const app = express();
 const PORT = 8080;
 
+// app setup
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: true}));
 
+// app data
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
+// app functions
+function generateRandomString(length) {
+  let result = '';
+  const chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+  const charLength = chars.length;
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * charLength))
+  }
+  return result;
+}
+
+// route GET requests
 app.get('/', (req, res) => {
   res.send("Hello!");
 });
@@ -18,12 +34,23 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars)
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get('/urls/:id', (req, res) => {
   const id = req.params.id;
   const templateVars = { id, longURL: urlDatabase[id] };
   res.render("urls_show", templateVars)
 })
 
+// route POST requests
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('ok')
+})
+
+// server listen request
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`)
 })
